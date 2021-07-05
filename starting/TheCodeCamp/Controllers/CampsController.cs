793ae49c87b85714aac4ba19c6538c1590b614b8
Darwin <dgu@netcompany.com>
2021-07-05 17:18:36 +0200

    Chapter 3 tutorial

---
+++ starting/TheCodeCamp/Controllers/CampsController.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using TheCodeCamp.Data;
+using TheCodeCamp.Models;
+
+namespace TheCodeCamp.Controllers
+{
+    public class CampsController : ApiController
+    {
+        private readonly ICampRepository _repository;
+        private readonly IMapper _mapper;
+
+        public CampsController(ICampRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+        // GET: Camps
+        public async Task<IHttpActionResult> Get()
+        {
+            try
+            {
+                var result = await _repository.GetAllCampsAsync();
+
+                //Mapping
+                var mappedResult = _mapper.Map<IEnumerable<CampModel>>(result);
+                return Ok(mappedResult);
+            }
+            catch (Exception ex)
+            {
+                // TODO Add Logging
+                return InternalServerError(ex);
+            }
+        }
+    }
+}
---
+++ starting/TheCodeCamp/Data/CampMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TheCodeCamp.Models;
+
+namespace TheCodeCamp.Data
+{
+    public class CampMappingProfile : Profile
+    {
+        public CampMappingProfile()
+        {
+            CreateMap<Camp, CampModel>();
+        }
+    }
+}
